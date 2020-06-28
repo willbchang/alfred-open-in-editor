af9d4ca54155b78b7c4bac052b3b3be1a7c78de6
Will B Chang <will.b.chang@gmail.com>
2020-06-28 15:18:26 +0800

    fix: open workflow on empty selection

--- script.rb
+++ script.rb
@@ -6,4 +6,5 @@ filepath = ARGV[0]
 editor = ARGV[1]
 # Remove single quote around file path when selecting from Alfred File Browser
 filepath = /^'.*'$/.match?(filepath) ? filepath[1..-2] : filepath
-system "open -a #{editor.shellescape} #{filepath.shellescape}"
+system "open -a #{editor.shellescape} " +
+       "#{filepath.shellescape if File.exist?(filepath)}"
