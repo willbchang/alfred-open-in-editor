3daded0999e19b8b207c08b53891d56f88298e71
Will B Chang <will.b.chang@gmail.com>
2020-07-13 15:11:23 +0800

    docs: update comments

--- emacs.rb
+++ emacs.rb
@@ -11,17 +11,17 @@ filepath = query.gsub(/^'|'$/, '')
 # emacsclient call fail.
 filepath = File.exist?(filepath) ? filepath.shellescape : ''
 
-# If emacs server is not active or emacs is not active, open emacs
-# otherwise open filepath with emacs.
-# Without emacs client, it will lauch multiple Emacs instance instead of
-# one Emacs with multiple window.
-
 # https://www.gnu.org/software/emacs/manual/html_node/emacs/emacsclient-Options.html
 # -c: --create-frame
 # If filepath is a directory, open it in a new frame(window)
 create_frame = File.directory?(filepath) ? '-c' : ''
 
+# Without emacs client, it will lauch multiple Emacs instance instead of
+# one Emacs with multiple window.
 cmd = "/usr/local/bin/emacsclient #{create_frame} -n #{filepath}"
+
+# If emacs server is not active or emacs is not active, open emacs
+# otherwise open filepath with emacs.
 unless system cmd
   `open -a Emacs #{filepath}`
 end
