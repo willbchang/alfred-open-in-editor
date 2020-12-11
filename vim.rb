54722631a63b37c9f35ef624be196299fb27e081
Will B Chang <will.b.chang@gmail.com>
2020-12-11 08:57:55 +0800

    style: fix typo

--- vim.rb
+++ vim.rb
@@ -8,11 +8,11 @@ terminal = ARGV[2]
 filepath = query.gsub(/^'|'$/, "")
 
 # If it's not a file path, return empty string.
-# Avoid special character when running shell ommand.
+# Avoid special character when running shell command.
 filepath = File.exist?(filepath) ? filepath.shellescape : ""
 
 # If there isn't a vim instance running in Terminal,
-# or the filpath is not empty, run a vim instance with filepath.
+# or the filepath is not empty, run a vim instance with filepath.
 # Else, open the terminal app.
 if `ps | grep '[v]im'`.empty? || filepath != ""
   `osascript -e 'tell app "#{terminal}" to do script "#{vim} #{filepath}" activate'`
